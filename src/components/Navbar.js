612147e1779fdb35b7c85526f26f32015eba0a4d
Michal <michal.p.zarzycki@wp.pl>
2021-02-15 01:12:35 +0100

    Created Navbar and Sidebar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            NAVBAR
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div>
+            SIDEBAR
+        </div>
+    )
+}
+
+export default Sidebar
