5ad4f3a4c25187e683a2e7e4c11448c8bf15cb0e
Michal <michal.p.zarzycki@wp.pl>
2021-02-16 01:08:59 +0100

    Created Bar component with chart.js

---
+++ src/components/charts/Bar.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import Chart from "chart.js";
+import styles from './Bar.module.css';
+
+export default function Bar() {
+    const chartRef = React.createRef();
+    useEffect(()=>{
+        const myChartRef = chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: "bar",
+            data: {
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+            }
+        });
+    }, [])
+  
+        return (
+            <div className={styles.mainDiv}>
+                <canvas
+                    id="myChart"
+                    ref={chartRef}
+                />
+            </div>
+        )
+    
+}
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Bar from '../components/charts/Bar'
 import Line from '../components/charts/Line'
 
 function Dashboard() {
     return (
         <div>
           <Line />
+          <Bar />
         </div>
     )
 }
