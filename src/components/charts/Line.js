b93508c89c24035a26e68a2e1b136e480dd1272d
Michal <michal.p.zarzycki@wp.pl>
2021-02-17 23:39:28 +0100

    Added style to Line chart(removed label and x-axis text)

--- src/components/charts/Line.js
+++ src/components/charts/Line.js
@@ -6,19 +6,39 @@ export default function Line() {
     const chartRef = React.createRef();
     useEffect(()=>{
         const myChartRef = chartRef.current.getContext("2d");
-        
-        new Chart(myChartRef, {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+        gradientStroke.addColorStop(0, "#80b6f4");
+        gradientStroke.addColorStop(0.2, "#94d973");
+        gradientStroke.addColorStop(0.5, "#fad874");
+        gradientStroke.addColorStop(1, "#f49080");
+                new Chart(myChartRef, {
             type: "line",
             data: {
-                labels: ["Jan", "Feb", "March"],
+                labels: ["Jan", "Feb", "March",1,1,1,1,1,1,1,1,1,],
                 datasets: [
-                    {
-                        label: "Sales",
-                        data: [86, 67, 91],
+                    {   label: false,
+                        fill: "start",
+                        borderColor:               gradientStroke,
+                        pointBorderColor:          gradientStroke,
+                        pointBackgroundColor:      gradientStroke,
+                        pointHoverBackgroundColor: gradientStroke,
+                        pointHoverBorderColor:     gradientStroke,
+                        data: [86, 90, 91,90,90,90,90,90],
                     }
                 ]
             },
             options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            display: false
+                        }
+                    }]
+                }
             }
         });
     }, [])
