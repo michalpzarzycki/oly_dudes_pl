2c721706b33a4f7687248cf9164832653809f280
Michal <michal.p.zarzycki@wp.pl>
2021-02-18 23:45:13 +0100

    Added props to InfoChart

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -4,10 +4,10 @@ import styles from './InfoBar.module.css'
 function InfoBar() {
     return (
         <div className={styles.mainDiv}>
-            <InfoChart />
-            <InfoChart />
-            <InfoChart />
-            <InfoChart />
+            <InfoChart header={"My current weight :"} info={"80kg"}/>
+            <InfoChart header={"My BMI :"} info={"100"}/>
+            <InfoChart header={"My closest goal :"} info={"75kg snatch"}/>
+            <InfoChart header={"My quote :"} info={"Never give up, motherfucker"}/>
         </div>
     )
 }
--- src/components/charts/InfoChart.js
+++ src/components/charts/InfoChart.js
@@ -1,11 +1,11 @@
 import styles from './InfoChart.module.css'
 
-function InfoChart() {
+function InfoChart({header, info}) {
     return (
         <div className={styles.mainDiv}>
-            <h1 className={styles.header}>Your current weight:</h1>
+            <h1 className={styles.header}>{header}</h1>
             <div className={styles.infoBox}>
-                <p className={styles.info}>80kg</p>
+                <p className={styles.info}>{info}</p>
             </div>
         </div>
     )
