fc77e8e0e41d8e9a652c77f739e6a9bed6d0df85
Michal <michal.p.zarzycki@wp.pl>
2021-02-20 02:39:05 +0100

    Created CloseButtonDialog

---
+++ src/components/CloseButtonDialog.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function CloseButtonDialog({open, handleClose}) {
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Do you wanna delete that goal?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Yes
+          </Button>
+          <Button onClick={handleClose} color="danger" autoFocus>
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/components/GraphClose.js
+++ src/components/GraphClose.js
@@ -1,9 +1,21 @@
+import {useState} from 'react'
 import CancelPresentationIcon from '@material-ui/icons/CancelPresentation';
 import Button from '@material-ui/core/Button';
+import CloseButtonDialog from './CloseButtonDialog';
 function GraphClose() {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={() => { }}>
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClickOpen}>
             <CancelPresentationIcon />
+            <CloseButtonDialog open={open} handleClose={handleClose} />
         </Button>
     )
 }
