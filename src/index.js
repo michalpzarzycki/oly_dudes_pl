a49b849d3d9d0326b8df0915ae376a2269c3130f
Michal <michal.p.zarzycki@wp.pl>
2021-02-27 16:49:13 +0100

    Created store and add a Provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
 
-    <React.StrictMode>
+const store = createStore(reducer)
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
- ,
+    </Provider>
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
