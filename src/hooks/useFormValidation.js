7cc33adf7f3372b42315026aacbc465819af3aac
Michal <michal.p.zarzycki@wp.pl>
2021-03-02 22:52:13 +0100

    Displaing error messages in TextFields

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -18,14 +18,11 @@ useEffect(() => {
     setErrors(() => validate(values))
 }, [values])
     const handleChange = (event) => {
-        console.log("UFV handlechange", event.target.value)
         setValues((values) => ({...values, [event.target.name]: event.target.value}))
     }
     const handleSubmit = (event) => {
-      console.log("USE FORM VALIDATION ERRORS", errors)
         event.preventDefault();
         setIsSubmit(true)
     }
-
     return { handleSubmit, handleChange, errors, values}
 }
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -11,7 +11,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {useFormValidation} from '../hooks/useFormValidation'
+import { useFormValidation } from '../hooks/useFormValidation'
 import registerValidate from '../validate/registerValidate'
 
 const useStyles = makeStyles((theme) => ({
@@ -34,105 +34,109 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 function Register() {
-    const { handleChange, handleSubmit, errors, values} = useFormValidation(signUpUser, registerValidate)
-    const classes = useStyles();
-    function signUpUser() {
-      console.log("USERSIGNEDUP")
-    }
-    
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
+  const { handleChange, handleSubmit, errors, values } = useFormValidation(signUpUser, registerValidate)
+  const classes = useStyles();
+  function signUpUser() {
+    console.log("USERSIGNEDUP")
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
           </Typography>
-          <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  error={errors.firstName}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                  error={errors.lastName}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  error={errors.email}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  error={errors.password}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                error={errors.firstName}
+                onChange={handleChange}
+                helperText={errors.firstName}
+              />
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign Up
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                error={errors.lastName}
+                onChange={handleChange}
+                helperText={errors.lastName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                error={errors.email}
+                onChange={handleChange}
+                helperText={errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={errors.password}
+                onChange={handleChange}
+                helperText={errors.password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
             </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
                 </Link>
-              </Grid>
             </Grid>
-          </form>
-        </div>
-      </Container>
-    );
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
 
 export default Register
