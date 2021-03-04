d2dc6194403ea084630e08be738083ffe95720c8
Michal <michal.p.zarzycki@wp.pl>
2021-03-04 15:44:24 +0100

    Added dispatch cb to facebook-signin

--- src/firebase/facebook-signin.js
+++ src/firebase/facebook-signin.js
@@ -1,14 +1,18 @@
 import firebase from './firebase';
+import { signInRequest, signInSuccess, singInFailure } from '../actions'
 
 
-export function signInWithFacebookPopup() {
+export function signInWithFacebookPopup(dispatch=()=>{}) {
+    dispatch(signInRequest())
     let provider = new firebase.auth.FacebookAuthProvider();
 
     return new Promise((resolve, reject) => {
         firebase.auth().signInWithPopup(provider)
         .then(data => {
+            dispatch(signInSuccess(data.user))
             resolve(data)
         }).catch(err => {
+            dispatch(singInFailure(err))
             reject(err)
 
         })
