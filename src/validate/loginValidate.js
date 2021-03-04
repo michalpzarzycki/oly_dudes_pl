e0ced85d3e897dbe77bd7f23342a205fc17b7b90
Michal <michal.p.zarzycki@wp.pl>
2021-03-04 15:37:21 +0100

    loginValidate small bug

--- src/validate/loginValidate.js
+++ src/validate/loginValidate.js
@@ -1,6 +1,6 @@
 export default function loginValidate(values) {
     let errors = {mail:'', pass:''}
-    (values)
+    
     if(values?.email?.length<=3)  errors.mail = "error"
     if(values?.password?.length<=3)  errors.pass = "error"
 
